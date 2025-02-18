192a5d46d5e397e80570c0884524f8d73d0802ad
vladimir <v.kuzmich98@gmail.com>
2025-02-18 16:30:27 +0300

    [AP-1] route for activity_categories

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\Http\Controllers\ActivityCategoryController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('activity_categories')->group(function () {
+    Route::get('/', [ActivityCategoryController::class, 'index']);
+    Route::get('/{id}', [ActivityCategoryController::class, 'show']);
+    Route::post('/', [ActivityCategoryController::class, 'store']);
+    Route::put('/{id}', [ActivityCategoryController::class, 'update']);
+    Route::delete('/{id}', [ActivityCategoryController::class, 'delete']);
+});
