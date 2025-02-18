86f5b016480fd53f029d2c129b6232714b5f0c53
vladimir <v.kuzmich98@gmail.com>
2025-02-18 17:28:29 +0300

    [AP-2] route for activity_types

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ActivityCategoryController;
+use App\Http\Controllers\ActivityTypeController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,4 +11,12 @@ Route::prefix('activity_categories')->group(function () {
     Route::post('/', [ActivityCategoryController::class, 'store']);
     Route::put('/{id}', [ActivityCategoryController::class, 'update']);
     Route::delete('/{id}', [ActivityCategoryController::class, 'delete']);
+});
+
+Route::prefix('activity_types')->group(function () {
+    Route::get('/', [ActivityTypeController::class, 'index']);
+    Route::get('/{id}', [ActivityTypeController::class, 'show']);
+    Route::post('/', [ActivityTypeController::class, 'store']);
+    Route::put('/{id}', [ActivityTypeController::class, 'update']);
+    Route::delete('/{id}', [ActivityTypeController::class, 'delete']);
 });
